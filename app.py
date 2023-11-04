5fc7ad5cb00150940b3ad1f13dcbcdf08cd5f361
Alex <alar2300@colorado.edu>
2023-11-04 21:23:49 +0000

    update to /db_test route

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ def hello_world():
 
 @app.route('/db_test')
 def testing():
-    conn = psycopg2.connect("your_db_url_here")
+    conn = psycopg2.connect("postgres://scrappy_db_user:oFBmcMPwf5TmH6EowkCoVDGqJt7PljDH@dpg-cl3b9pquuipc738ana7g-a/scrappy_db")
     conn.close()
     return "Database Connection Successful"
